9cc3c214e3997e696145eed872b2b8ac2dec50d0
Key-F <kirill2010.97@mail.ru>
2021-11-23 20:11:18 +0300

    Правки в main
    BufferedReader заменен на Scanner, добавлен блок try catch

--- src/com/company/lab/Main.java
+++ src/com/company/lab/Main.java
@@ -1,25 +1,29 @@
 package com.company.lab;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        InputStreamReader fileReader = new InputStreamReader(new FileInputStream(System.getProperty("user.dir") + "/file.txt"), "UTF-8");
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        int size = Integer.valueOf(bufferedReader.readLine());
+        FileReader fileReader;
+        try {
+            fileReader = new FileReader(System.getProperty("user.dir") + "/file.txt");
+        }
+        catch (IOException e) {System.out.println("Не удалось считать из файла"); return;}
+        Scanner in = new Scanner(fileReader);
+        int size = Integer.valueOf(in.next());
+        in.nextLine();
         Cache testCache = new Cache(size);
         String line;
-        while ((line = bufferedReader.readLine()) != null) { // Построчно читаем из файла и парсим нужные значения
+        while (in.hasNext()) { // Построчно читаем из файла и парсим нужные значения
+            line = in.nextLine();
             String value = line.substring(line.lastIndexOf(" ") + 1);
             String key = line.substring(line.indexOf(" ") + 1, line.lastIndexOf(" "));
             String type = line.substring(0, line.indexOf(" "));
             testCache.addRecord(key, value, type);
         }
-        bufferedReader.close();
+        fileReader.close();
 
         testCache.printCache(); // тестовый вывод на печать
 
